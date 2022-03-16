2531bb11b52086f934525f503b1bfa534a0fc101
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-15 20:37:04 -0500

    Update Employee.test.js

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,43 +1,28 @@
 const Employee = require("../lib/Employee");
 
 test('Creates Employee object with id, name, email', () => {
-    const e = new Employees();
-    expect(e.id).toEqual(expect.any(Number));
+    const e = new Employee('Mike', 67, 'mike@gmail.com');
     expect(e.name).toEqual(expect.any(String));
     expect(e.email).toEqual(expect.any(String));
+    expect(e.id).toEqual(expect.any(Number));
 });
 
+test('Can get name via getName()', () => {
+    const e = new Employee('Mike');
+    expect(e.getName()).toEqual('Mike');
+});
 
-    // describe("getName", () => {
-    //     it("Can get name via getName()", () => {
-    //         const testValue = "Ali";
-    //         const e = new Employee(testValue);
-    //         expect(e.getName()).toBe(testValue);
-    //     });
-    // });
-        
-    // describe("getId", () => {
-    //     it("Can get id via getId()", () => {
-    //         const testValue = 100;
-    //         const e = new Employee("Jo", testValue);
-    //         expect(e.getId()).toBe(testValue);
-    //     });
-    // });
-        
-    // describe("getEmail", () => {
-    //     it("Can get email via getEmail()", () => {
-    //         const testValue = "test@test.com";
-    //         const e = new Employee("Jo", 1, testValue);
-    //         expect(e.getEmail()).toBe(testValue);
-    //     });
-    // });
-        
-    // describe("getRole", () => {
-    //     it("getRole() should return \"Employee\"", () => {
-    //         const testValue = "Employee";
-    //         const e = new Employee("Ali", 1, "test@test.com");
-    //         expect(e.getRole()).toBe(testValue);
-    //     });
-    // });
+test('can get id via getID()', () => {
+    const e = new Employee('Mike', 67, 'mikey@gmail.com');
+    expect(e.getID()).toEqual(67);
+});
 
+test('can get email via getEmail()', () => {
+    const e = new Employee('Mike', 67, 'mikey@gmail.com');
+    expect(e.getEmail()).toEqual('mikey@gmail.com');
+});
 
+test('can get employee via getRole()', () => {
+    const e = new Employee('Mike', 67, 'mikey@gmail.com');
+    expect(e.getRole()).toEqual('Employee');
+});
